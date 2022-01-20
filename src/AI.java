1c73785f03b00e1100e5eec3edbaaa6a56bdaeba
Eric Kel <eric.kel@outlook.com>
2022-01-20 14:24:10 -0500

    Fixed simple AI.

--- src/AI.java
+++ src/AI.java
@@ -12,11 +12,11 @@ public class AI {
 
     public int[] easy() {
         int[] hitCoor = new int[2];
-        hitCoor[0] = (int) Math.random() * 10;
-        hitCoor[1] = (int) Math.random() * 10;
+        hitCoor[0] = (int) (Math.random() * 10);
+        hitCoor[1] = (int) (Math.random() * 10);
         while(Game.aiToUserBoardHits[hitCoor[0]][hitCoor[1]] == 1 || Game.aiToUserBoardHits[hitCoor[0]][hitCoor[1]] == 2) {
-            hitCoor[0] = (int) Math.random() * 10;
-            hitCoor[1] = (int) Math.random() * 10;
+            hitCoor[0] = (int) (Math.random() * 10);
+            hitCoor[1] = (int) (Math.random() * 10);
         }
         return hitCoor;
     }
