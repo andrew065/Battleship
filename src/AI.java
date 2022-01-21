b6a1219e7ae525a1b51e0fdf8bc07f6dc114fa12
Eric Kel <eric.kel@outlook.com>
2022-01-21 14:56:01 -0500

    Made progress on AI.

--- src/AI.java
+++ src/AI.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 
 public class AI {
     boolean lastShotHit = false;
+    int[][] shootGrid = new int[10][10];
+    static int[] currentCoor = {4, 4};
+    static int layer = 0;
+    boolean triedLeft = false;
+    boolean triedRight = false;
+    boolean triedDown = false;
+    boolean triedUp = false;
 
     public static int[] getCoords() {
         int[] coordinate = new int[2];
@@ -23,12 +30,45 @@ public class AI {
         return hitCoor;
     }
     public void medium() {}
-    public void hard() {
+    public int[] hard() {
         if(lastShotHit) {
-
+            return hunt();
+        } else {
+            if(layer == 0) {
+                layer++;
+                return currentCoor;
+            } else {
+                if(checkDoneLayer()) {
+                    layer++;
+                }
+                int[] possibleCoor = currentCoor;
+                possibleCoor[0]+=2;
+                possibleCoor[1]+=2;
+                while(!(currentCoor[0] <= layer && currentCoor[1] <= layer)) {
+                    possibleCoor = currentCoor;
+                    possibleCoor[0]-=-2;
+                    possibleCoor[1]-=-2;
+                }
+            }
         }
+        return currentCoor;
     }
-    public void hunt() {
 
+    private boolean checkDoneLayer() {
+        for(int i  = 0; i < shootGrid.length; i++) {
+            for(int j  = 0; j < shootGrid[i].length; j++) {
+                if(i <= layer && j <= layer && i % 2 == 0 && j % 2 == 0) {
+                    if(shootGrid[i][j] == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private int[] hunt() {
+        int[] huntCoor = currentCoor;
+        return huntCoor;
     }
 }
