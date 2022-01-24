1da33daa0cfc91a571848dc0392a482b4d68fb3a
Derrick <haderrick@gmail.com>
2022-01-23 22:42:56 -0500

    new

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -23,20 +25,42 @@ public class Leaderboard {
         }
     }
 
-//    public void updateLeaderboard() throws FileNotFoundException {
-//        user.loadData();
-//
-//        int winner = Game.hasUserOrAIWon();
-//        boolean hasUserWon = false;
-//        int cnt = 0;
-//        if(winner == 1) hasUserWon = true;
-//        else {
-//            for(int i = 0; i < Game.isAiShipSunk.length; i++) {
-//                if(Game.isAiShipSunk[i]) cnt++;
-//            }
-//        }
-//        user.updateData(hasUserWon ? 5 : cnt, hasUserWon);
-//    }
+    //run after every game
+    public void updateLeaderboard(boolean hasUserWon, boolean[] isAiShipSunk) throws FileNotFoundException {
+        user.loadData();
+        int cnt = 0;
+        for(int i = 0; i < isAiShipSunk.length; i++) {
+            if(isAiShipSunk[i]) cnt++;
+        }
+        user.updateData(hasUserWon ? 5 : cnt, hasUserWon);
+
+        Scanner currentUserScanner = new Scanner("Players/" + user.username + ".txt");
+        int thisUserWins = currentUserScanner.nextInt();
+
+        ArrayList<String> leaders= new ArrayList<String>();
+        File file = new File("Files/Leaderboard.txt");
+        Scanner LeaderboardScanner = new Scanner(file);
+        int LeaderboardNum = 0;
+        while(LeaderboardScanner.hasNext()) {
+            String s = LeaderboardScanner.nextLine();
+            leaders.add(s);
+            LeaderboardNum++;
+        }
+
+        leaders.add(user.username);
+        if(LeaderboardNum != 0) {
+            for (int i = 0; i < leaders.size(); i++) {
+                String cur = leaders.get(i);
+                File userFile = new File("Users/" + cur + ".txt");
+                Scanner sc = new Scanner(userFile);
+                int numGames = sc.nextInt();
+                if (numGames < thisUserWins)
+                    Collections.swap(leaders, leaders.indexOf(cur), leaders.indexOf(user.username));
+            }
+        }
+        PrintWriter p = new PrintWriter("Files/Leaderboard.txt");
+        for(String s : leaders) p.println(s);
+    }
 
     public void loadLeaderboard () throws FileNotFoundException {
         if (hasLeaderboard) {
